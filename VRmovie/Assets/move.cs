8a799a7228e441f9c7fb6e30a60503aa2d1e87fc
森口大雅 <gucchi512@outlook.jp>
2018-04-07 13:44:58 +0900

    プロトタイプ(仮)
    大まかな形

---
+++ VRmovie/Assets/move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public float speed=1.0f;
+    Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos.z += speed;
+        transform.position = pos;
+    }
+}
