8884b28a5d15c168898f74ca4f8c03e893db8f42
森口大雅 <gucchi512@outlook.jp>
2018-04-16 09:54:17 +0900

    夏完成(仮)
    夏の場面、フェードイン/アウト完成

---
+++ VRmovie/Assets/Scripts/SceneController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    GameObject fadepanel;
+    FadeController fade;
+    // Use this for initialization
+    void Start() {
+        fadepanel = GameObject.FindWithTag("panel");
+        fade = fadepanel.GetComponent<FadeController>();
+        Debug.Log(fadepanel);
+        Debug.Log(fade);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!fade.isFadeOut) {
+            fade.isFadeOut = true;
+        }
+
+    }
+}
